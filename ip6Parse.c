8a73594607eedc864a72279e159ff72f5ae0ea81
Sam Trenholme <maradns@samiam.org>
2020-08-18 17:40:00 -0700

    Make parser strict
    If there is no :: in the string, we need to have eight “quad” sets of hex
    numbers
    
    If there is :: in the string, we can not have more than seven “quad”s
    
    If there are : characters in the string, we can have a maximum of four hex
    digits between (or before/after) : characters in the string
    
    If there is no : in the string, we must have precisely 32 hex digits

--- ip6Parse.c
+++ ip6Parse.c
@@ -17,6 +17,10 @@
 	the first character in the string, -2 the second one, etc.)
 	-256: The input string is too long
 	-257: There are too many hex digits or colons in the input string
+	-258: Unexpected error occurred parsing "::" sequence
+	-259: Too many colons
+	-260: No colons and we do not have 32 hex digits in string
+	-261: More than four hex digits in a row and ":" present in string
 
    Rules: Single colon separates 16-bit (4 hex digit) numbers
           Double colon (only one allowed or error) means 
@@ -49,12 +53,14 @@ int ip6Parse(char *human, unsigned char *ip6) {
 	int currentQuad = 0;
 	int currentHexDigit = 0;
 	char *humanStart = human;
+	int hexCount = 0;
+	int colonCount = 0;
 
  	char last = 0;
 	int index = 0;
 
 	// Pass 1: See if we have a double colon and count the colons
-        // after the double colon
+        // after the double colon.  Also: Count colons
 	while(*human != 0 && index < 100) {
 		if(last == ':' && *human == ':') {
 			if(doubleColonIndex != -1) {
@@ -65,6 +71,7 @@ int ip6Parse(char *human, unsigned char *ip6) {
 		if(last != ':' && *human == ':' && doubleColonIndex != -1) {
 			afterDoubleColonQuads++;
 		}
+		if(*human == ':') { colonCount++; }
 		last = *human;
 		human++;
 		index++;
@@ -98,29 +105,55 @@ int ip6Parse(char *human, unsigned char *ip6) {
 			thisQuad <<= 4;
 			thisQuad += thisHex;
 			currentHexDigit++;
+			// Maximum 4 hex digits between colons
+			if(currentHexDigit == 5 && colonCount != 0) {
+				return -261;
+			}
+			hexCount++;
 		}
 
 		// Single colon processing: End current Quad
 		if(*human == ':' && index != doubleColonIndex &&
-				currentHexDigit != 0) {
-			currentHexDigit = 4;
+				currentHexDigit != 0 && currentHexDigit != 4) {
+			currentHexDigit = 8;
+		}
+		if(*human == ':' && index != doubleColonIndex &&
+				currentHexDigit == 4) {
+			currentHexDigit = 0;
+		}
+
+		if(*human == ':' && index == doubleColonIndex) {
+			if(thisQuad != 0 || currentHexDigit != 0) { 
+				return -258; 
+			}
+			if(currentQuad + afterDoubleColonQuads >= 8) {
+				return -259; // Too many colons
+			}
+			currentQuad = 8 - afterDoubleColonQuads;
+			if(currentQuad < 0) { return -259; }
+			outIndex = currentQuad * 2;
 		}
 
 		human++;
 		// Convert a series of up to four hex digits in to raw IPv6
-		if(currentHexDigit == 4 || *human == 0) {
+		if((currentHexDigit == 4 && thisHex != -1) || 
+				currentHexDigit == 8 || *human == 0) {
 			if(outIndex + 1 >= 16) { return -257; }
 			ip6[outIndex + 1] = thisQuad & 0xff;
 			ip6[outIndex] = thisQuad >> 8;
 			outIndex += 2;
 			thisQuad = 0;
-			currentHexDigit = 0;
+			if(currentHexDigit == 8) {
+				currentHexDigit = 0;
+			}
 			currentQuad++;
 		}
 
 		index++;
 	}
 	if(index >= 100) { return -256; /* Error */ }
+	if(colonCount == 0 && hexCount != 32) { return -260; }
+	if(colonCount != 0 && currentQuad != 8) { return -262; }
 
 	return 1; // Success	
 }
@@ -130,13 +163,15 @@ int ip6Parse(char *human, unsigned char *ip6) {
 int main(int argc, char **argv) {
 	char *test;
 	uint8_t ip6[16];
+	int result;
 	if(argc > 1) {
 		test = argv[1];
 	} else {
 		test = "2001-0db8-1234-5678 0000-0000-0000-0005";
 	}
-	if(ip6Parse(test, ip6) != 1) {
-		puts("ip6Parse returned syntax error");
+	result = ip6Parse(test, ip6);
+	if(result != 1) {
+		printf("ip6Parse returned error code %d\n",result);
 		return 0;
 	}
         printf("%02x%02x-%02x%02x-%02x%02x-%02x%02x " 
